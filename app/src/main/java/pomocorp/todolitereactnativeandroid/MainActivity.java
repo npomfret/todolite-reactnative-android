6f9d2522f0de57b5b0492be4f75d8ddcc19a6bfa
npomfret <nick-github@snowmonkey.co.uk>
2016-01-04 15:08:07 +0000

    added activity code and some build stuff

--- app/src/main/java/pomocorp/todolitereactnativeandroid/MainActivity.java
+++ app/src/main/java/pomocorp/todolitereactnativeandroid/MainActivity.java
@@ -1,52 +1,137 @@
 package pomocorp.todolitereactnativeandroid;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.KeyEvent;
 
-public class MainActivity extends AppCompatActivity {
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.View;
+import com.couchbase.lite.android.AndroidContext;
+import com.couchbase.lite.javascript.JavaScriptViewCompiler;
+import com.couchbase.lite.listener.Credentials;
+import com.couchbase.lite.listener.LiteListener;
+import com.couchbase.lite.util.Log;
+import com.facebook.react.LifecycleState;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+
+public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
+
+    private final String TAG = "TodoLite";
+    private static final int DEFAULT_LISTEN_PORT = 5984;
+    private int listenPort;
+    private Credentials allowedCredentials;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Log.d(TAG, "onCreate method called");
+
+        // 1
+        mReactRootView = new ReactRootView(this);
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModuleName("index.android")
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+        mReactRootView.startReactApplication(mReactInstanceManager, "TodoLite-ReactNative-Android", null);
+
+        setContentView(mReactRootView);
+        initCBLite();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void initCBLite() {
+        try {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            // 2
+            allowedCredentials = new Credentials("", "");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            // 3
+            View.setCompiler(new JavaScriptViewCompiler());
+
+            // 4
+            AndroidContext context = new AndroidContext(this);
+            Manager.enableLogging(Log.TAG, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_SYNC, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_QUERY, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_VIEW, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_CHANGE_TRACKER, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_BLOB_STORE, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_DATABASE, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_LISTENER, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_MULTI_STREAM_WRITER, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_REMOTE_REQUEST, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_ROUTER, Log.VERBOSE);
+            Manager manager = new Manager(context, Manager.DEFAULT_OPTIONS);
+
+            // 5
+            listenPort = startCBLListener(DEFAULT_LISTEN_PORT, manager, allowedCredentials);
+
+            Log.i(TAG, "initCBLite() completed successfully with: " + String.format(
+                    "http://%s:%s@localhost:%d/",
+                    allowedCredentials.getLogin(),
+                    allowedCredentials.getPassword(),
+                    listenPort));
+
+        } catch (final Exception e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    private int startCBLListener(int listenPort, Manager manager, Credentials allowedCredentials) {
+        LiteListener listener = new LiteListener(manager, listenPort, allowedCredentials);
+        int boundPort = listener.getListenPort();
+        Thread thread = new Thread(listener);
+        thread.start();
+        return boundPort;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onResume(this);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager.showDevOptionsDialog();
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
     }
 }
